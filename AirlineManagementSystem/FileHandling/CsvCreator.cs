bc13643ef7add11a35fe77d7d260aca1b71016e1
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-18 17:47:53 +0400

    Added csv creator, takes folder, file and header to create each entity csv

---
+++ AirlineManagementSystem/FileHandling/CsvCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineManagementSystem.FileHandling
+{
+    internal class CsvCreator
+    {
+        public static void CreateCsv(string folderName, string fileName, string[] headers)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating {fileName}.csv: {ex.Message}");
+            }
+        }
+    }
+}
