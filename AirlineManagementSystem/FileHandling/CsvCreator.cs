8654bafd6a57ce6375f4119ddfb6c3198a90d658
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-18 18:24:10 +0400

    Implemented data folder and csv file creator with the right paths

--- AirlineManagementSystem/FileHandling/CsvCreator.cs
+++ AirlineManagementSystem/FileHandling/CsvCreator.cs
@@ -10,6 +10,32 @@ namespace AirlineManagementSystem.FileHandling
         {
             try
             {
+                // because by default the current dir is where the app compiled bin\Debug\net10.0, we escape 3 times to fix it 
+                // Path.combine handles the path correctly regarding the platform 
+                string folderPath = Path.Combine("..", "..", "..", folderName);
+                string filePath = Path.Combine(folderPath, $"{fileName}.csv");
+
+                //Check if the directory exist, if not it creates it
+                if (! Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                    Console.WriteLine($"Successfully created directory: {folderName}");
+                }
+                else
+                {
+                    Console.WriteLine($"{folderName} folder already created.");
+                }
+
+                //Check if the file exist, if not it creates it
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath);
+                    Console.WriteLine($"Successfully created file: {fileName}");
+                }
+                else
+                {
+                    Console.WriteLine($"{fileName}.csv already created.");
+                }
 
             }
             catch (Exception ex)
