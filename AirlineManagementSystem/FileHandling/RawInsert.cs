5f2f656ea8969d354dfd64e1ec34493ae90b325e
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-19 09:09:59 +0400

    Implemented a method to insert new raws into existing entity

---
+++ AirlineManagementSystem/FileHandling/RawInsert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineManagementSystem.FileHandling
+{
+    internal class RawInsert
+    {
+        public static void AddRaw(string fileName, string[] values)
+        {
+            /// <summary>
+            /// Add new raw of values to existing entity 
+            /// </summary>
+
+            //Create the actual path for the csv file
+            string filePath = Path.Combine("..", "..", "..", "Data", $"{fileName}.csv");
+            //create raw line 
+            string rawLine = string.Join(',', values);
+
+            //check if the file exist 
+            if (File.Exists(filePath))
+            {
+                //check if the entity created (header line)
+                StreamReader sr = new StreamReader(filePath);
+                string headerLine = sr.ReadLine() ?? "";
+                sr.Close();
+                if (headerLine != "")
+                {
+                    //create strean writer but with append to not delete existing line
+                    StreamWriter sw = new StreamWriter(filePath, append: true);
+                    sw.WriteLine(rawLine);
+                    sw.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{fileName} entity is not created. ");
+            }
+        }
+    }
+}
