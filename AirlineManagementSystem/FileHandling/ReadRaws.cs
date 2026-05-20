ba1642e01b426cacd809d7bf7bf0b6e0a71a4665
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-20 12:00:54 +0400

    Implemented a method that return the raw with primary key in dictionary format

--- AirlineManagementSystem/FileHandling/ReadRaws.cs
+++ AirlineManagementSystem/FileHandling/ReadRaws.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using AirlineManagementSystem.HelperFunctions;
+using System;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using System.Text;
 
 namespace AirlineManagementSystem.FileHandling
 {
     internal class ReadRaws
     {
-         
+        internal static Dictionary<string, string> ReadRawByPK(string fileName, string primaryKey, bool isBinDirectory = false)
+        {
+            try
+            {
+                Dictionary<string, string> record = new Dictionary<string, string>();
+
+                string filePath = Path.Combine("Data", $"{fileName}.csv");
+                if (! isBinDirectory) { filePath = Path.Combine("..", "..", "..", filePath); }
+
+                string headerLine = LinesRead.ReadLine(filePath);
+                string raw = LinesRead.ReturnLineByWord(filePath, primaryKey);
+
+                string[] headers = headerLine.Split(",");
+                string[] values = raw.Split(",");
+
+                for (int i= 0; i < headers.Length && i < values.Length; i++)
+                {
+                    record.Add(headers[i], values[i]);
+                }
+
+                return record;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading {fileName}: {ex.Message}");
+                return new Dictionary<string, string>();
+            }
+        }
     }
 }
