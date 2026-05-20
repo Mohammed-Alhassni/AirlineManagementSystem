c91da2a8ceca6271705bb3ccd7f1d1170a1bfb92
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-20 11:03:54 +0400

    Implemented helper methods one read spcific line other reads all the lines

---
+++ AirlineManagementSystem/HelperFunctions/LinesRead.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace AirlineManagementSystem.HelperFunctions
+{
+    internal class LinesRead
+    {
+        internal static string ReadLine(string filePath, int lineNumber = 1)
+        {
+            if (!File.Exists(filePath) || lineNumber < 1)
+            {
+                return "";
+            }
+
+            // Using 'using' ensures the file is closed properly even if an error happens
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string currentLine = "";
+
+                for (int i = 1; i <= lineNumber; i++)
+                {
+                    currentLine = sr.ReadLine() ?? "";
+
+                    // If the file ends before we reach the requested line
+                    if (currentLine == "")
+                    {
+                        return "";
+                    }
+                }
+
+                return currentLine;
+            }
+        }
+
+        internal static List<string> ReadAllLine(string filePath)
+        {
+            List<string> allLines = new List<string>();
+
+            if (!File.Exists(filePath)){ return []; }
+
+            // Using 'using' ensures the file is closed properly even if an error happens
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                //flag is true till the lines finish
+                string line;
+
+                while ((line = sr.ReadLine() ?? "") != "")
+                {
+                    allLines.Add(line);
+                }
+            }
+
+            return allLines;
+        }
+    }
+}
