d32bc7000a4da364422e8e3cb63c340d84491fc2
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-05-25 15:16:42 +0400

    Implemented entity methods that extract variables names from an entity class and return them as pascalcase or space seperated for future use.

---
+++ AirlineManagementSystem/HelperFunctions/EntityMethods.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+public class EntityMethods
+{
+    internal static List<string> ExtractVaribles<T>(bool cleanString = true) where  T : new()
+    {
+        List<string> variblesNames = new List<string>();
+        // Get all public properties of the class
+        Type type = typeof(T);
+        PropertyInfo[] properties = type.GetProperties();
+
+
+        foreach (PropertyInfo prop in properties)
+        {
+            string name = prop.Name;
+                
+            if (cleanString)
+            {
+                name = Regex.Replace(name, "(?<!^)(?=[A-Z])", " ");
+            }
+                
+            variblesNames.Add(name);
+        }
+
+        return variblesNames;
+    }
+}
