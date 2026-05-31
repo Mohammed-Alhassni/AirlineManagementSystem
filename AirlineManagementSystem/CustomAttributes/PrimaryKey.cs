4fdb1f46908317768bf4be5cb11dc32c37f5a711
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-31 17:01:40 +0400

    Added primary key custom attributes that flags the property to be checked for pk constraints

---
+++ AirlineManagementSystem/CustomAttributes/PrimaryKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineManagementSystem.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Property ,AllowMultiple = false, Inherited = true)]
+    internal class PrimaryKeyAttribute : Attribute
+    {
+
+    }
+}
