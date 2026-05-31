d0ace425427910e69128938190ab3a92f7867c1b
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-31 17:00:34 +0400

    Added foreign key attributes that holds information of which table and column is linked to the property

---
+++ AirlineManagementSystem/CustomAttributes/ForeignKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineManagementSystem.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple =false, Inherited = true)]
+    internal class ForeignKeyAttribute : Attribute
+    {
+        public string LinkedTable { get; }
+        public string LinkedColumn { get; }
+
+        // Constructor forces you to provide these details when using the attribute
+        public ForeignKeyAttribute(string linkedTable, string linkedColumn)
+        {
+            LinkedTable = linkedTable;
+            LinkedColumn = linkedColumn;
+        }
+    }
+}
