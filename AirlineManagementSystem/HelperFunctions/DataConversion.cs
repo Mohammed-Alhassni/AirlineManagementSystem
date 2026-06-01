4528250ff99a0dd2fa8bf3f6fc583a7211132bd6
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-01 17:36:53 +0400

    Implemented a Data conversion class that takes array of values and return a dictionary with every value paried with header

---
+++ AirlineManagementSystem/HelperFunctions/DataConversion.cs
@@ -0,0 +1,38 @@
+﻿using AirlineManagementSystem.FileHandling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineManagementSystem.HelperFunctions
+{
+    internal class DataConversion
+    {
+        internal static Dictionary<string, string> CreateDictionary(string fileName, string[] values, bool isBinDirectory = false)
+        {
+            try
+            {
+                Dictionary<string, string> record = new Dictionary<string, string>();
+
+                string filePath = Path.Combine("Data", $"{fileName}.csv");
+                if (!isBinDirectory) { filePath = Path.Combine("..", "..", "..", filePath); }
+
+                string headerLine = LinesRead.ReadLine(filePath);
+
+                string[] headers = headerLine.Split(",");
+
+                for (int i = 0; i < headers.Length && i < values.Length; i++)
+                {
+                    record.Add(headers[i], values[i]);
+                }
+
+                return record;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading {fileName}: {ex.Message}");
+                return new Dictionary<string, string>();
+            }
+        }
+
+    }
+}
