42e5060d2473bd11bb00cc2155d6457b2c7f87dd
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-01 17:42:03 +0400

    Implemented a method that checks fk before adding it, achived by mapping values into the class of entity

---
+++ AirlineManagementSystem/HelperFunctions/WriteRaws.cs
@@ -0,0 +1,33 @@
+﻿using AirlineManagementSystem.FileHandling;
+using AirlineManagementSystem.IntegrityValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineManagementSystem.HelperFunctions
+{
+    /// <summary>
+    /// Safely adding values into entities after checking the primary and forign key 
+    /// </summary>
+    internal class WriteRaws
+    {
+        internal static void InsertValues<T>(string fileName, string[] values, bool isBinDirectory = false) where T : new()
+        {
+            try
+            {
+                Dictionary<string, string> ValuesDictionary=  DataConversion.CreateDictionary(fileName, values);
+
+                T TargetObj = EntityMapper.MapToEntity<T>(ValuesDictionary);
+
+                FKValidate.ValidateFK(TargetObj);
+
+                LineInsert.AddLine(fileName, values, isBinDirectory);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
