2c62f78e8321edb57f0a29d747ca9e5434cb3072
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-09 09:38:51 +0400

    Implemented a menu for passenger portal with options still not implemented

--- AirlineManagementSystem/UserInterface/PassengerPortal.cs
+++ AirlineManagementSystem/UserInterface/PassengerPortal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AirlineManagementSystem.UIHelpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,8 +9,71 @@ namespace AirlineManagementSystem.UserInterface
     {
         internal static void ShowPassengerPortal()
         {
+            string[] options = { "Browse & Search Flights", "Book a Ticket", "Manage My Tickets", "My Profile", "Personalized Recommendations", "Logout" };
+            int currentSelection = 0;
+            bool interacting = true;
+
+            // Hide the blinking cursor 
+            Console.CursorVisible = false;
+
+            while (interacting)
+            {
+                Console.Clear();
+
+                // Pass the currentSelection so the box knows which line to highlight
+                MenuElements.RenderMenuBox(
+                    "Admin Dashboard",
+                    "Welcome back. Select an option to proceed.",
+                    options,
+                    "Use UP/DOWN arrows to navigate, ENTER to select.",
+                    currentSelection
+                );
+
+                // Intercept keypress without echoing the character to the console
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        currentSelection--;
+                        // Wrap around to the bottom option if navigating past the top
+                        if (currentSelection < 0) currentSelection = options.Length - 1;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        currentSelection++;
+                        // Wrap around to the top option if navigating past the bottom
+                        if (currentSelection >= options.Length) currentSelection = 0;
+                        break;
+
+                    case ConsoleKey.Enter:
+                        interacting = false;
+                        break;
+                }
+            }
+
+            // Restore the cursor before leaving the menu context
+            Console.CursorVisible = true;
             Console.Clear();
-            Console.WriteLine("This is Passnger screen");
+
+            // 10. Handle the action based on what they selected
+            ExecuteMenuAction(options[currentSelection]);
+        }
+
+        private static void ExecuteMenuAction(string selection)
+        {
+            switch (selection)
+            {
+                case "":
+
+                    break;
+                case " ":
+
+                    break;
+                case "Logout":
+                    WelcomeScreen.ShowWelcomeScreen();
+                    break;
+            }
         }
     }
 }
