09277c08569329fbd2454444742866a7764573c2
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-06-11 13:38:22 +0400

    checkpoint

--- AirlineManagementSystem/UserInterface/PassengerPortal.cs
+++ AirlineManagementSystem/UserInterface/PassengerPortal.cs
@@ -1,4 +1,5 @@
-﻿using AirlineManagementSystem.UIHelpers;
+﻿using AirlineManagementSystem.HelperFunctions;
+using AirlineManagementSystem.UIHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +13,24 @@ namespace AirlineManagementSystem.UserInterface
             string[] options = { "Browse & Search Flights", "Book a Ticket", "Manage My Tickets", "My Profile", "Personalized Recommendations", "Logout" };
             int currentSelection = 0;
             bool interacting = true;
+            List<string> tieresRank = new List<string> { "Bronze", "Silver", "Gold"};
+
+            Dictionary<string, string> recentTicket = ReadRaws.ReadRawByWord("ticket", user["passenger_id"]);
+
+            List <KeyValuePair<string, string>> userInfo = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string> ("Name", user["full_name"]),
+                new KeyValuePair<string, string> ("Loyality Tier", user["tier_status"]),
+                new KeyValuePair<string, string> ("Points Balance", user["loyalty_points_balance"]),
+            };
+
+            List<KeyValuePair<string, string>> recentTicketInfo = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string> ("Seat Class", recentTicket["seat_class"]),
+                new KeyValuePair<string, string> ("Booking Date", recentTicket["booking_date_time"]),
+                new KeyValuePair<string, string> ("Earned Loyality Points", recentTicket["loyalty_points_earned"]),
+                new KeyValuePair<string, string> ("Seat Class", recentTicket["seat_class"])
+            };
 
             // Hide the blinking cursor 
             Console.CursorVisible = false;
@@ -28,6 +47,10 @@ namespace AirlineManagementSystem.UserInterface
                     "Use UP/DOWN arrows to navigate, ENTER to select.",
                     currentSelection
                 );
+
+                MenuElements.DisplaySummary("summary", userInfo, 4);
+                MenuElements.DisplaySummary("Recenet Ticket", recentTicketInfo, 4);
+                MenuElements.DisplaySummary("summary", userInfo, 4);
 
                 // Intercept keypress without echoing the character to the console
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
